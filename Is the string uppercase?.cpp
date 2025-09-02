4731823762bc153925cdf79ddaf9d36bfa52a8e5
mhmd2900 <mohamedsad2900@gmail.com>
2025-09-02 09:53:10 +0300

    Update Is the string uppercase?.cpp

--- Is the string uppercase?.cpp
+++ Is the string uppercase?.cpp
@@ -1,21 +1,19 @@
+#include<iostream>
+#include<string>                                       //better to include string           // better not use namespace
+#include<cctype>
 
 
+bool is_uppercase ( const std::string &s )  //bool return //good funct name //const as not editable //& avoid string many copies 
+{
+for (size_t i = 0 ; i< s.size() ; i++ )  //size_t compare to unsigned s.size  //s.size standard than size(s) //i !=0 not safe
+      if ( islower(s[i]))  // != 1 is wrong
+      return false ;   // no needed { } to close for
+return true ;
+}
 
 
-
-
-
-
-
-string str ="ACSKLDFJSGSKLDFJSKLDFJ" ;
- int x = 0 ;
-for ( int i = 0 ; i < size(str) ; i ++ )
-  {
-    if ( isupper(str[i]) )
-    continue ;
-
-    else 
-    x ++ ;
-    
-  }
-
+int main ()
+{
+std::cout << is_uppercase ("ACSKLDFJSGSKLD JSKLDFJ") << std::endl;
+return 0 ;
+}
