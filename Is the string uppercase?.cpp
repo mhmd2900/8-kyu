8b610250ab264b52906b0ad40ac9be8b8a36c8cc
mhmd2900 <mohamedsad2900@gmail.com>
2025-09-02 09:58:00 +0300

    Update Is the string uppercase?.cpp

--- Is the string uppercase?.cpp
+++ Is the string uppercase?.cpp
@@ -11,9 +11,20 @@ for (size_t i = 0 ; i< s.size() ; i++ )  //size_t compare to unsigned s.size  //
 return true ;
 }
 
+bool is_upper ( const std::string &s ) 
+{
+for ( auto i : s)     // range-based for loop , direct iteration with ch not indices 
+      if ( islower(i)) 
+      return false ;  
+return true ;
+}
+
+
+
 
 int main ()
 {
 std::cout << is_uppercase ("ACSKLDFJSGSKLD JSKLDFJ") << std::endl;
+std::cout << is_upper ("ACSKLDFJSGSKLD JSKLDFJ") << std::endl;
 return 0 ;
 }
