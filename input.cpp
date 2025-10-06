74098e0963a61e3d2fcf1519e9bcb746c8955955
mhmd2900 <mohamedsad2900@gmail.com>
2025-10-06 21:14:25 +0300

    input.cpp to input.cpp

---
+++ input.cpp
@@ -0,0 +1,57 @@
+
+//////////////    input number
+float read_number ( string message )
+{ float f ;
+cout << message ;
+while ( true)
+{
+    if ( cin >> f )
+    {
+    return f ; 
+    }
+
+    else 
+    {
+    cout << " invalid number ,, plz reenter \n " ;
+    cin.clear();
+    cin.ignore( 10000 , '\n');
+    }
+}  
+}
+
+
+
+
+///////////////////    input string ( charchters )
+
+string read ( string message )
+{
+    string password ;
+    char ch ;
+    cout << message ;
+    while (true)
+    {
+        while ( cin.get(ch) )
+        {
+        if ( ch == '\r')
+        continue ;
+        if ( ch == '\n')
+        break ;
+        password += ch ;
+        }
+
+        if (! password.empty())
+        return password ;
+
+        else 
+        {
+        cout << " empty password , not acceted , repeat \n" ;
+        cin.clear();
+        }
+    }
+}
+
+
+
+                                                                                }
+
