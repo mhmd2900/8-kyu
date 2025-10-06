71baa4a924da434305b0d203d41181a76f858e91
mhmd2900 <mohamedsad2900@gmail.com>
2025-10-06 21:26:43 +0300

    input.cpp

--- input.cpp
+++ input.cpp
@@ -1,55 +1,64 @@
-
-//////////////    input number
-float read_number ( string message )
-{ float f ;
-cout << message ;
-while ( true)
-{
-    if ( cin >> f )
-    {
-    return f ; 
-    }
-
-    else 
-    {
-    cout << " invalid number ,, plz reenter \n " ;
-    cin.clear();
-    cin.ignore( 10000 , '\n');
-    }
-}  
-}
+                                                                                                                                            //////////////     input random
+                                                                                                                                            int random ( int from , int to )
+                                                                                                                                            {
+                                                                                                                                              return rand()%( to - from + 1)+from ;
+                                                                                                                                            }
 
 
 
 
-///////////////////    input string ( charchters )
 
-string read ( string message )
-{
-    string password ;
-    char ch ;
-    cout << message ;
-    while (true)
-    {
-        while ( cin.get(ch) )
-        {
-        if ( ch == '\r')
-        continue ;
-        if ( ch == '\n')
-        break ;
-        password += ch ;
-        }
-
-        if (! password.empty())
-        return password ;
-
-        else 
-        {
-        cout << " empty password , not acceted , repeat \n" ;
-        cin.clear();
-        }
-    }
-}
+                                                                                                                                            //////////////    input number
+                                                                                                                                            float read_number ( string message )
+                                                                                                                                            { float f ;
+                                                                                                                                            cout << message ;
+                                                                                                                                            while ( true)
+                                                                                                                                            {
+                                                                                                                                                if ( cin >> f )
+                                                                                                                                                {
+                                                                                                                                                return f ; 
+                                                                                                                                                }
+                                                                                                                                            
+                                                                                                                                                else 
+                                                                                                                                                {
+                                                                                                                                                cout << " invalid number ,, plz reenter \n " ;
+                                                                                                                                                cin.clear();
+                                                                                                                                                cin.ignore( 10000 , '\n');
+                                                                                                                                                }
+                                                                                                                                            }  
+                                                                                                                                            }
+                                                                                                                                            
+                                                                                                                                            
+                                                                                                                                            
+                                                                                                                                            
+                                                                                                                                            ///////////////////    input string ( charchters )
+                                                                                                                                            
+                                                                                                                                            string read ( string message )
+                                                                                                                                            {
+                                                                                                                                                string password ;
+                                                                                                                                                char ch ;
+                                                                                                                                                cout << message ;
+                                                                                                                                                while (true)
+                                                                                                                                                {
+                                                                                                                                                    while ( cin.get(ch) )
+                                                                                                                                                    {
+                                                                                                                                                    if ( ch == '\r')
+                                                                                                                                                    continue ;
+                                                                                                                                                    if ( ch == '\n')
+                                                                                                                                                    break ;
+                                                                                                                                                    password += ch ;
+                                                                                                                                                    }
+                                                                                                                                            
+                                                                                                                                                    if (! password.empty())
+                                                                                                                                                    return password ;
+                                                                                                                                            
+                                                                                                                                                    else 
+                                                                                                                                                    {
+                                                                                                                                                    cout << " empty password , not acceted , repeat \n" ;
+                                                                                                                                                    cin.clear();
+                                                                                                                                                    }
+                                                                                                                                                }
+                                                                                                                                            }
 
 
 
