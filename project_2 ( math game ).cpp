5aa310f38dec30687fd884ba00e3057b760a4feb
mhmd2900 <mohamedsad2900@gmail.com>
2025-10-18 19:25:42 +0300

    project_2 ( math game ).cpp

--- project_2 ( math game ).cpp
+++ project_2 ( math game ).cpp
@@ -4,14 +4,14 @@
 #include<ctime>
 using namespace std ;
 
-enum enquestion_level { easy = 1 , med = 2 , hard = 3 , mix = 4 };
+enum enquestion { easy = 1 , med = 2 , hard = 3 , mix = 4 };
 enum enoperator { add = 1 , sub = 2 , mult = 3 , divi = 4 , all = 5 };
-enum enpass_fail { pass = 1 , fail = 2 , draw = 3 } ;
+enum enpassfail { pass = 1 , fail = 2 , draw = 3 } ;
 
-string level_to_word (enquestion_level enenquestion_level )
+string level_to_word (enquestion enenquestion )
 {
 string word[4] = { "easy" , "med" , "hard" , "mix"};
-return word [ enenquestion_level - 1 ];
+return word [ enenquestion - 1 ];
 }
 
 string operator_to_word (enoperator enenoperator )
@@ -35,9 +35,9 @@ return "all" ;
 
 struct stgamestats 
 {
-enpass_fail enpasser ;
-int questions_number ;
-enquestion_level enenquestion_level ;
+enpassfail enpasser ;
+int questions_number = 0 ;
+enquestion enenquestion ;
 enoperator enenoperator ;
 int correct_number = 0 ;
 int wrong_number = 0  ;
@@ -47,8 +47,8 @@ struct sttwo_numbers
 {
 int num1 = 0 ;
 int num2  = 0 ;
-int ans = 0 ;
-int yans = 0 ;
+int comp_ans = 0 ;
+int user_ans = 0 ;
 char op = 'a' ;
 };
 
@@ -74,7 +74,7 @@ char op = 'a' ;
                                                                                 return rand()%( to - from + 1 ) + from ;
                                                                                 }
 
-                                                                                int random2 ()
+                                                                                int random_op ()
                                                                                 {
                                                                                     int t = 0 ;
                                                                                  while ( true )
@@ -110,25 +110,24 @@ cin.ignore(100,'\n');
 }
 }
 
-
-void choose_level ( sttwo_numbers& ststtwo_numbers , stgamestats ststgamestats)   
+void choose_nums ( sttwo_numbers& ststtwo_numbers , stgamestats ststgamestats)   
 {
-if (ststgamestats.enenquestion_level == enquestion_level::easy )
+if (ststgamestats.enenquestion == enquestion::easy )
 {
 ststtwo_numbers.num1 = random ( 1 , 9);
 ststtwo_numbers.num2 = random ( 1 , 9);
 }
-else if (ststgamestats.enenquestion_level == enquestion_level::med)
+else if (ststgamestats.enenquestion == enquestion::med)
 {
 ststtwo_numbers.num1 = random ( 11 , 99);
 ststtwo_numbers.num2 = random ( 11 , 99);
 }
-else if (ststgamestats.enenquestion_level == enquestion_level::hard )
+else if (ststgamestats.enenquestion == enquestion::hard )
 {
 ststtwo_numbers.num1 = random ( 111 , 222);
 ststtwo_numbers.num2 = random ( 111 , 222 );
 }
-else if (ststgamestats.enenquestion_level == enquestion_level::mix )
+else if (ststgamestats.enenquestion == enquestion::mix )
 {
 ststtwo_numbers.num1 = random ( 1 , 222);
 ststtwo_numbers.num2 = random ( 1 , 222);
@@ -146,36 +145,35 @@ ststtwo_numbers.op = '*';
 else if (ststgamestats.enenoperator == enoperator::divi)
 ststtwo_numbers.op = '/';
 else if (ststgamestats.enenoperator == enoperator::all)
-ststtwo_numbers.op = char ( random2()) ;
-
+ststtwo_numbers.op = char ( random_op()) ;
 }
 
-void show_equation (sttwo_numbers&  ststtwo_numbers)
+void user_answer (sttwo_numbers&  ststtwo_numbers)
 {
 
 cout << ststtwo_numbers.num1 << endl ;
 cout << ststtwo_numbers.op  << endl ; 
 cout << ststtwo_numbers.num2 << endl  ;
-cout << "_______    \n " ;
-ststtwo_numbers.yans = number ( " ??      ") ;
+cout << "_______ \n " ;
+ststtwo_numbers.user_ans = number ( "") ;
 
 }
 
 void comp_answer ( sttwo_numbers& ststtwo_numbers)
 {
 if         (ststtwo_numbers.op == '+' )
-ststtwo_numbers.ans = ststtwo_numbers.num1 + ststtwo_numbers.num2 ;
+ststtwo_numbers.comp_ans = ststtwo_numbers.num1 + ststtwo_numbers.num2 ;
 else if    (ststtwo_numbers.op == '-' )
-ststtwo_numbers.ans = ststtwo_numbers.num1 - ststtwo_numbers.num2 ;
+ststtwo_numbers.comp_ans = ststtwo_numbers.num1 - ststtwo_numbers.num2 ;
 else if    (ststtwo_numbers.op == '*' )
-ststtwo_numbers.ans = ststtwo_numbers.num1 * ststtwo_numbers.num2 ;
+ststtwo_numbers.comp_ans = ststtwo_numbers.num1 * ststtwo_numbers.num2 ;
 else if    (ststtwo_numbers.op == '/' )
-ststtwo_numbers.ans = ststtwo_numbers.num1 / ststtwo_numbers.num2 ;
+ststtwo_numbers.comp_ans = ststtwo_numbers.num1 / ststtwo_numbers.num2 ;
 }
 
-void compare ( sttwo_numbers ststtwo_numbers , stgamestats& ststgamestats )
+void correct_wrong ( sttwo_numbers ststtwo_numbers , stgamestats& ststgamestats )
 {
-if ( ststtwo_numbers.yans == ststtwo_numbers.ans )
+if ( ststtwo_numbers.user_ans == ststtwo_numbers.comp_ans )
 {
 cout << " \n correct answer \n" ;
 system ( " color 2F");
@@ -183,93 +181,91 @@ ststgamestats.correct_number ++ ;
 }
 else 
 {
-cout << " \n wrong answer \n the correct answer is " << ststtwo_numbers.ans << endl ; 
+cout << " \n wrong answer \n the correct answer is " << ststtwo_numbers.comp_ans << endl ; 
 system ( " color 4F");
-ststgamestats.wrong_number++;
+ststgamestats.wrong_number ++ ;
 }
 }
 
-void each_round ( int count , stgamestats& ststgamestats)
+void round ( int count , stgamestats& ststgamestats)
 {
-
 sttwo_numbers ststtwo_numbers ;
 
 cout << "  \n ============================== \n       Question [" << count << "/"<< ststgamestats.questions_number << "]" << endl ;
 
-choose_level    ( ststtwo_numbers , ststgamestats) ;
+choose_nums     ( ststtwo_numbers , ststgamestats) ;
 choose_operator ( ststtwo_numbers , ststgamestats) ;
-show_equation   ( ststtwo_numbers) ;
+user_answer     ( ststtwo_numbers) ;
 comp_answer     ( ststtwo_numbers) ;
-compare         ( ststtwo_numbers , ststgamestats) ;
-
-
+correct_wrong   ( ststtwo_numbers , ststgamestats) ;
 }
 
-void who_pass ( stgamestats& ststgamestats )
+void pass_or_fail ( stgamestats& ststgamestats )
 {
 if      ( ststgamestats.correct_number > ststgamestats.wrong_number)
-ststgamestats.enpasser = enpass_fail::pass ;
+ststgamestats.enpasser = enpassfail::pass ;
 else if ( ststgamestats.correct_number < ststgamestats.wrong_number)
-ststgamestats.enpasser = enpass_fail::fail ;
+ststgamestats.enpasser = enpassfail::fail ;
 else
-ststgamestats.enpasser = enpass_fail::draw ;
+ststgamestats.enpasser = enpassfail::draw ;
 }
 
-void final_result ( stgamestats ststgamestats )
+void print__pass_or_fail ( stgamestats ststgamestats )
 {
-cout << "_________________________________________________________________ \n";
+cout << "_________________________________________________________________\n";
 cout << " final result is ";
-if      ( ststgamestats.enpasser == enpass_fail::pass)
+if      ( ststgamestats.enpasser == enpassfail::pass)
 {
-cout << " PASS  :-)   \n ________________________________________ \n ";
+cout << " PASS  :-)   \n_________________________________________________________________\n ";
 system (" color 2F") ;
 }
 
-else if ( ststgamestats.enpasser == enpass_fail::fail)
+else if ( ststgamestats.enpasser == enpassfail::fail)
 {
-cout << " FAIL  :-(   \n ________________________________________ \n " ;
+cout << " FAIL  :-(   \n_________________________________________________________________\n " ;
 system ( " color 4F");
 }
 
 else 
 {
-" DRAW \n ________________________________________________________________ \n " ;
+" DRAW \n_________________________________________________________________\n " ;
 system ("color 6 F");
 }
+"_________________________________________________________________\n" ;
 }
 
 void stats ( stgamestats ststgamestats )
 {
 cout << " \n number of questions   : " << ststgamestats.questions_number ;
-cout << " \n question level        : " << level_to_word   (ststgamestats.enenquestion_level) ;
+cout << " \n question level        : " << level_to_word   (ststgamestats.enenquestion) ;
 cout << " \n operative type        : " << operator_to_word(ststgamestats.enenoperator);
 cout << " \n correct answers       : " << ststgamestats.correct_number ;
 cout << " \n wrong answers         : " << ststgamestats.wrong_number ;
 }
 
-void game_brief ()
+void game ()
 {
 stgamestats  ststgamestats ;
 ststgamestats.questions_number   = number (" \n how many questions do you want to answer ? \n") ;
-ststgamestats.enenquestion_level = enquestion_level (number (" choose question level    [1]easy  [2]med  [3]hard  [4]mix  \n") ) ;
+ststgamestats.enenquestion = enquestion (number (" choose question level    [1]easy  [2]med  [3]hard  [4]mix  \n") ) ;
 ststgamestats.enenoperator       = enoperator       (number (" choose operator  [1]add  [2]sub  [3]mult  [4]divi   [5]all  \n") ) ;
 
 for ( int i = 1 ; i <= ststgamestats.questions_number ; i ++ )
-each_round (i , ststgamestats);
+round (i , ststgamestats);
 
-who_pass(ststgamestats) ;
-final_result (ststgamestats) ;
+pass_or_fail(ststgamestats) ;
+print__pass_or_fail (ststgamestats) ;
 stats ( ststgamestats) ;
 
 }
 
-void game_start()
+void restart()
 {
 do
 {
 system ("cls");
 system ( "color 0F") ;
-game_brief();
+game();
 } while ( want_to_repeat());
 }
 
@@ -277,9 +273,7 @@ game_brief();
 int main ()
 {
 srand ( (unsigned)time(NULL));
-game_start ( ) ;
+restart ( ) ;
 
 return 0 ;
 }
-
-int main ()
