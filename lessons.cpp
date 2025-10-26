b2f036090ce18ac76c5564528e933cb60b0b9972
mhmd2900 <mohamedsad2900@gmail.com>
2025-10-26 19:17:31 +0300

    Update lessons.cpp

--- lessons.cpp
+++ lessons.cpp
@@ -1,8 +1,10 @@
 int a = 5 ;
 float d = 2.0 ;
+int u = 50 ;
 double f = 4.123456 ;
 printf(" the a = %d \n " ,  a ) ;   // 5
-printf(" also a equals = %0*d \n \n  " , 5 , a ) ; // 00005
+printf(" also a equals = %0*d \n \n  " , 2  , a ) ; // 05
+printf(" also a equals = %0*d \n \n  " , 2  , u ) ; // 50
 
 printf ( " the d = %.4f \n" ,   d );  // 2.0000
 printf ( " the d = %.4f \n" ,   f );   // 4.1235
