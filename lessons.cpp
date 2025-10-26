1e2d990b666a7e359d448cf6253356d615ec1750
mhmd2900 <mohamedsad2900@gmail.com>
2025-10-26 18:16:28 +0300

    lessons.cpp

---
+++ lessons.cpp
@@ -0,0 +1,8 @@
+
+printf(" the a = %d \n " ,  a ) ;   // 5
+printf(" also a equals = %0*d \n \n  " , 5 , a ) ; // 00005
+
+printf ( " the d = %.4f \n" ,   d );  // 2.0000
+printf ( " the d = %.4f \n" ,   f );   // 4.1235
+printf ( " and the f equals = %.*f \n" , 4 , d );  // 2.0000
+printf ( " and the f equals = %.*f \n \n" , 4 , f );  // // 4.1235
