814fef108b9f27e512ae00301f673d7bde3c0cc1
mhmd2900 <mohamedsad2900@gmail.com>
2025-10-27 16:16:11 +0300

    Update lessons.cpp

--- lessons.cpp
+++ lessons.cpp
@@ -21,9 +21,9 @@ double f = 4.123456 ;
 printf(" the a = %d \n " ,  a ) ;   // 5
 printf(" also a equals = %0*d \n \n  " , 2  , a ) ; // 05
 printf(" also a equals = %0*d \n \n  " , 2  , u ) ; // 50
-printf(" also a equals = %02d \n \n  " , u ) ; // 50
+printf(" also a equals = %02d \n \n  " , u ) ; // 50              *********
 
-printf ( " the d = %.4f \n" ,   d );  // 2.0000
+printf ( " the d = %.4f \n" ,   d );  // 2.0000                  **********
 printf ( " the d = %.4f \n" ,   f );   // 4.1235
 printf ( " and the f equals = %.*f \n" , 4 , d );  // 2.0000
 printf ( " and the f equals = %.*f \n \n" , 4 , f );  // // 4.1235
